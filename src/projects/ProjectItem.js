b471a079141b473e1d991610f672de308702f1d2
Cyril Gavala <cyril.gavala@gmail.com>
2021-11-15 18:22:21 +0100

    ---Version 2.0.0--- - updated projects' images - updated info in data.json - slightly changes page layout - improved page responsiveness

--- src/projects/ProjectItem.js
+++ src/projects/ProjectItem.js
@@ -1,8 +1,8 @@
 export default function ProjectItem(props) {
 
     return <div className={"projects-item"}>
-        <div className={"projects-item-headline"}><a href={props.url} target={"_blank"} rel={"noopener noreferrer"}>{props.name}</a></div>
-        <img className={"projects-item-image"} src={props.image} alt={"Project screenshot"}/>
-        <div className={"projects-item-description"}>{props.description}</div>
+            <img className={"projects-item-image"} src={props.image} alt={"Project screenshot"}/>
+            <div className={"projects-item-headline"}><a href={props.url} target={"_blank"} rel={"noopener noreferrer"}>{props.name}</a></div>
+            <div className={"projects-item-description"}>{props.description}</div>
     </div>
 }
--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -1,28 +1,15 @@
 import ProjectItem from "./ProjectItem";
-import {useState} from "react";
 
 export default function Projects(props) {
 
-    const [index, setIndex] = useState(0);
     const projects = props.data.projects.map(project => {
         return <ProjectItem name={project.name} url={project.url} image={project.image} description={project.description}/>
     })
 
-    const nextProject = () => {
-        setIndex((index + 1) % projects.length)
-    }
-
-    const previousProject = () => {
-        setIndex((index + projects.length - 1) % 3)
-    }
-
     return <div id={"projects-container"}>
         <h2>My projects</h2>
         <div id={"projects-slider"}>
-            <div id={"previous"} onClick={previousProject}><img src={"../images/icons/previous_icon.png"} alt={"Previous project"}/></div>
-            {projects[index]}
-            <div id={"next"} onClick={nextProject}><img src={"../images/icons/next_icon.png"} alt={"Next project"}/>
-            </div>
+            {projects}
         </div>
     </div>
 }
