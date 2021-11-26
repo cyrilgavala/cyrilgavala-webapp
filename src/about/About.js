7f08da7fbcfd4255ec1ff1247961075a7508c971
Cyril Gavala <cyril.gavala@gmail.com>
2021-11-26 23:16:25 +0100

    ---Version 2.0.3--- - slightly styling update

--- src/about/About.js
+++ src/about/About.js
@@ -2,7 +2,6 @@ export default function About(props) {
 
     return <div id={"about-container"}>
         <div id={"photo-wrapper"}>
-            <div id={"blur"}/>
             <img src={props.data.pathToProfilePicture} alt={"Profile"}/>
         </div>
         <div id={"bio-wrapper"}>
