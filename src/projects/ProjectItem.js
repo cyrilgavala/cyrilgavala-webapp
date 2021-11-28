cf729a4b55f1fdb888e17610f39cb2877928852f
Cyril Gavala <cyril.gavala@gmail.com>
2021-11-28 16:07:16 +0100

    ---Version 2.1.0--- - styling update - added new project

--- src/projects/ProjectItem.js
+++ src/projects/ProjectItem.js
@@ -1,8 +1,9 @@
 export default function ProjectItem(props) {
 
-    return <div className={"projects-item"}>
-            <img className={"projects-item-image"} src={props.image} alt={"Project screenshot"}/>
-            <div className={"projects-item-headline"}><a href={props.url} target={"_blank"} rel={"noopener noreferrer"}>{props.name}</a></div>
-            <div className={"projects-item-description"}>{props.description}</div>
+    return <div className={"projects-item"} style={{"background-image": "url(" + props.image + ")", "background-size": "cover", "background-position": "center"}}>
+        <div className={"projects-item-description"}>{props.description}</div>
+        {/*<img className={"projects-item-image"} src={props.image} alt={"Project screenshot"}/>*/}
+        <div className={"projects-item-headline"}><a href={props.url} target={"_blank"}
+                                                     rel={"noopener noreferrer"}>{props.name}</a></div>
     </div>
 }
--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -3,13 +3,12 @@ import ProjectItem from "./ProjectItem";
 export default function Projects(props) {
 
     const projects = props.data.projects.map(project => {
-        return <ProjectItem name={project.name} url={project.url} image={project.image} description={project.description}/>
+        return <ProjectItem name={project.name} url={project.url} image={project.image}
+                            description={project.description}/>
     })
 
     return <div id={"projects-container"}>
         <h2>My projects</h2>
-        <div id={"projects-slider"}>
-            {projects}
-        </div>
+        {projects}
     </div>
 }
