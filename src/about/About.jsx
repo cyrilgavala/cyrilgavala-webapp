29d89c748c748bddaf4470fac672dbbfb916bccd
Cyril Gavala <cyril.gavala@gmail.com>
2022-04-12 22:04:20 +0200

    ---Version 3.0.5--- - tuned up some animations

--- src/about/About.jsx
+++ src/about/About.jsx
@@ -2,8 +2,6 @@ export default function About(props) {
 
     return <div id={"about-container"}>
         <div id={"photo-wrapper"}/>
-        <div id={"bio-wrapper"}>
-            <p>{props.data.bio}</p>
-        </div>
+        <div id={"bio-wrapper"}>{props.data.bio}</div>
     </div>
 }
